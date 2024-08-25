9e08a7c4fca7e9748a70ce35627e4c0a6b9e88a1
Gonzalo Romero <yayox588@gmail.com>
2024-08-25 01:52:36 -0400

    mejoras visuales

--- static/js/index.js
+++ static/js/index.js
@@ -1,40 +1,45 @@
-let db_peliculas = [];
 
 function carga_datos() {
     fetch("/pelicula/getAll")
         .then(response => response.json())
         .then(data => {
-            db_peliculas = data;
-            console.log("Data received:", db_peliculas);
-            mostrar_datos();
+            peliculas = data;
+            mostrar_datos(peliculas);
         })
         .catch(error => console.error('Error data:', error));
 }
 
 
-function mostrar_datos() {
+function mostrar_datos(data) {
     var divBody = document.getElementById("containerCard");
     divBody.innerHTML = ''
-    db_peliculas.forEach((e) => {
-        let div2 = document.createElement("div");
+    data.forEach((e) => {
+        const div2 = document.createElement("div");
         div2.className = "col-12 col-lg-3 mb-4";
-        let divcard = document.createElement('div');
-        divcard.className = "card align-items-center justify-content-center";
-        let caratula = document.createElement("img");
+
+        const divcard = document.createElement('div');
+        divcard.className = "container-fluid card align-items-center justify-content-center";
+
+        const caratula = document.createElement("img");
         /*caratula.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvTs8O9rWjP0cWAxVHelhq4kBUUxkOTtzreA&s";*/
         caratula.src = e.linkCaratula;
         divcard.appendChild(caratula);
-        let cardbody = document.createElement("div");
-        let cardTitulo = document.createElement("h5");
+
+        const divContenedor = document.createElement("div");
+        divContenedor.className = "container d-flex align-items-center"
+        
+        const cardbody = document.createElement("div");
+        cardbody.className = "mx-auto text-center"
+        const cardTitulo = document.createElement("h5");
         cardTitulo.innerText = e.nombre;
         cardbody.appendChild(cardTitulo);
-        let genero = document.createElement("p");
-        genero.innerText = e.genero;
-        cardbody.appendChild(genero);
-        let masdetalles = document.createElement("button");
+
+        const masdetalles = document.createElement("button");
+        masdetalles.className = "btn px-5 btn-light uppercase"
         masdetalles.innerText = "ver";
         cardbody.appendChild(masdetalles);
-        divcard.appendChild(cardbody);
+        divContenedor.appendChild(cardbody);
+        divcard.appendChild(divContenedor);
         div2.appendChild(divcard)
         divBody.appendChild(div2);
     });
@@ -50,7 +55,7 @@ window.addEventListener("load",function(){
         itemNav.className = "nav-item dropdown";
 
         let linkNav = document.createElement("button");
-        linkNav.className = "btn btn-secondary dropdown-toggle";
+        linkNav.className = "btn btn-light dropdown-toggle";
         linkNav.setAttribute("data-bs-toggle","dropdown");
         linkNav.setAttribute("aria-expanded","false");
         linkNav.innerText = usuariolog;
@@ -82,7 +87,7 @@ window.addEventListener("load",function(){
         /*  <a class="nav-link" href="/login">Login</a> */
         divLog.innerHTML = "";
         let linkLogin = document.createElement("button");
-        linkLogin.className ="btn btn-primary";
+        linkLogin.className ="btn btn-light px-3 me-2";
         linkLogin.id = "btn-login"
         var iconoLogin = "<i class='bx bx-user-circle' ></i>";
         linkLogin.innerHTML = iconoLogin + "  Login"
